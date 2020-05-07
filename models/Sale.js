bab05c6c111bc8523c93b97db8b4f79bb3b47729
BannedWolf <dsapaiv@gmail.com>
2020-05-07 17:47:16 +0300

    Excluded sensitive config.env file

--- models/Sale.js
+++ models/Sale.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 const saleSchema = new mongoose.Schema({
     storeName: {
         type: String,
-        
-    }
+        minlength: 2,
+        maxlength: 16,
+        required: true
+    },
+    description: {
+        type: String,
+        minlength: 5,
+        maxlength: 128,
+        required: true
+    },
+    image: String,
+    
 })
