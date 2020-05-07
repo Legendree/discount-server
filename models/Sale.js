efec9d717654e985a4b20d77d683e3101e8ab832
BannedWolf <dsapaiv@gmail.com>
2020-05-07 19:47:33 +0300

    Added more field to the model

--- models/Sale.js
+++ models/Sale.js
@@ -14,6 +14,20 @@ const saleSchema = new mongoose.Schema({
         maxlength: 128,
         required: true
     },
-    image: String,
-    
+    image: {
+        type: String,
+        default: 'no-image.jpg',
+        required: [true, 'Image of the sale is a required attribute']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    expiresAt: {
+        type: Date,
+        required: [true, 'Please enter experation date for this sale']
+    },
+    storeColor: Number,
 })
+
+module.exports = mongoose.model('Sale', saleSchema);
