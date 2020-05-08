943f33f2abb7a2045bee0a48d0009794c9dcffda
BannedWolf <dsapaiv@gmail.com>
2020-05-08 18:25:04 +0300

    Fixed a warning for collection.ensureIndex

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,7 +20,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 
   try {
-    //VERIFY TOKEN
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = await User.findById(decoded.id);
