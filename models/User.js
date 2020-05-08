754852120da383303238bee4032bbe12c41497da
Dan <Danmil511@gmail.com>
2020-05-08 16:17:43 +0300

    Added user model and some packages

---
+++ models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email',
+    ],
+    required: [true, 'Please enter your email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//ENCRYPT PASSWORD
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//SIGN JWT AND RETURN
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET);
+};
+
+//MATCH USER ENTERED PASSWORD TO HASHED PASSWORD
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//GENERATE AND HASH PASSWORD TOKEN
+userSchema.methods.getResetPasswordToken = function () {
+  //GENERATE TOKEN
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  //HASH TOKEN AND SET TO resetPasswordToken FIELD
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  console.log(this.resetPasswordToken);
+
+  //SET THE EXPIRE
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+
+module.exports = mongoose.model('User', userSchema);
