d287f2f8799cf5bca33296b520327c1b49910c44
Dan <Danmil511@gmail.com>
2020-05-08 16:19:45 +0300

    Fixed Post to post

--- routes/posts.js
+++ routes/posts.js
@@ -5,8 +5,7 @@ const Post = require('../models/Posts');
 const ErrorResponse = require('../utils/errorResponse');
 
 const asyncHandler = require('express-async-handler');
-const advanceQuery = require('../middleware/advancedQuery')
-
+const advanceQuery = require('../middleware/advancedQuery');
 
 const router = express.Router();
 
@@ -14,18 +13,22 @@ router.get(
   '/',
   advanceQuery(Post),
   asyncHandler(async (req, res, next) => {
-    res.status(200).json({ success: true, count: res.advancedResult.length, data: res.advancedResult });
+    res.status(200).json({
+      success: true,
+      count: res.advancedResult.length,
+      data: res.advancedResult,
+    });
   })
 );
 
 router.get(
   '/:id',
   asyncHandler(async (req, res, next) => {
-    const Post = await Post.findById(req.params.id);
-    if (!Post) return next(new ErrorResponse('Post not found', 404));
+    const post = await Post.findById(req.params.id);
+    if (!post) return next(new ErrorResponse('Post not found', 404));
     res.status(200).json({
       success: true,
-      data: Post,
+      data: post,
     });
   })
 );
@@ -33,29 +36,29 @@ router.get(
 router.post(
   '/',
   asyncHandler(async (req, res, next) => {
-    const Post = await Post.create(req.body);
+    const post = await Post.create(req.body);
     res.status(200).json({
       success: true,
-      data: Post,
+      data: post,
     });
   })
 );
 
 router.put('/:id', async (req, res, next) => {
-  const Post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
-  if (!Post) return next(new ErrorResponse('Post not found', 404));
+  if (!post) return next(new ErrorResponse('Post not found', 404));
   res.status(200).json({
     success: true,
-    data: Post,
+    data: post,
   });
 });
 
 router.delete('/:id', async (req, res, next) => {
-  const Post = await Post.findByIdAndDelete(req.params.id);
-  if (!Post) return next(new ErrorResponse('Post not found', 404));
+  const post = await Post.findByIdAndDelete(req.params.id);
+  if (!post) return next(new ErrorResponse('Post not found', 404));
   res.status(200).json({
     success: true,
     data: {},
