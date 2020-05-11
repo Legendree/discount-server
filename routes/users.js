dee88af7e0c24f5fb3c85e870d966682b39ef992
Dan <Danmil511@gmail.com>
2020-05-11 17:39:30 +0300

    Added all routes to the user route

--- routes/users.js
+++ routes/users.js
@@ -24,4 +24,58 @@ router.get(
   })
 );
 
+router.get(
+  '/:id',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    if (!user) return next(new ErrorResponse('User not found', 404));
+    res.send({
+      success: true,
+      data: user,
+    });
+  })
+);
+
+router.post(
+  '/',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body);
+    res.send({
+      success: true,
+      data: user,
+    });
+  })
+);
+
+router.put(
+  '/:id',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) return next(new ErrorResponse('User not found', 404));
+    res.send({
+      success: true,
+      data: user,
+    });
+  })
+);
+
+router.delete(
+  '/:id',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return next(new ErrorResponse('User not found', 404));
+    res.send({
+      success: true,
+      data: user,
+    });
+  })
+);
+
 module.exports = router;
