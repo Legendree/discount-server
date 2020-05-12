d2a69a23c0b36cda377bcf6c2508086fc4436822
Dan <Danmil511@gmail.com>
2020-05-12 11:50:29 +0300

    Added file upload and a small fix

--- routes/posts.js
+++ routes/posts.js
@@ -12,7 +12,6 @@ const role = require('../middleware/role');
 
 const router = express.Router();
 
-
 // @desc    Get all available posts
 // @route   GET /api/v1/posts
 // @access  Public
@@ -48,7 +47,7 @@ router.get(
 // @access  Private
 router.post(
   '/',
-  [auth, role('user')],
+  [auth, role('admin')],
   asyncHandler(async (req, res, next) => {
     const post = await Post.create(req.body);
     res.status(200).json({
@@ -58,25 +57,64 @@ router.post(
   })
 );
 
-router.put('/:id', async (req, res, next) => {
-  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  if (!post) return next(new ErrorResponse('Post not found', 404));
-  res.status(200).json({
-    success: true,
-    data: post,
-  });
-});
+router.put(
+  '/:id',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!post) return next(new ErrorResponse('Post not found', 404));
+    res.status(200).json({
+      success: true,
+      data: post,
+    });
+  })
+);
 
-router.delete('/:id', async (req, res, next) => {
-  const post = await Post.findByIdAndDelete(req.params.id);
-  if (!post) return next(new ErrorResponse('Post not found', 404));
-  res.status(200).json({
-    success: true,
-    data: {},
-  });
-});
+router.delete(
+  '/:id',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) return next(new ErrorResponse('Post not found', 404));
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  })
+);
+
+router.put(
+  '/:id/photo',
+  [auth, role('admin')],
+  asyncHandler(async (req, res, next) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) next(new ErrorResponse('The post does not exist', 404));
+
+    if (!req.files) next(new ErrorResponse('Please add a photo', 400));
+
+    const file = req.files.file;
+    // Make sure image is a photo
+    if (!file.mimetype.startsWith('image')) {
+      return next(new ErrorResponse('Please upload an image file', 400));
+    }
+    file.name = `photo_${post._id}${path.parse(file.name).ext}`;
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+      if (err) {
+        console.log(err);
+        return next(new ErrorResponse('Problem with file upload', 500));
+      }
+
+      await Post.findByIdAndUpdate(req.params.id, { image: file.name });
+
+      res.json({
+        success: true,
+        data: file.name,
+      });
+    });
+  })
+);
 
 module.exports = router;
