93e8b2f30613907d4cff741a53132701c2701147
Dan <Danmil511@gmail.com>
2020-05-15 14:31:41 +0300

    Fixed send email

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -1,16 +1,15 @@
-//const nodemailer = require('nodemailer');
 const mailgun = require('mailgun-js');
 
-const mg = mailgun({
-  apiKey: `${process.env.MAILGUN_APIKEY}`,
-  domain: `${process.env.MAILGUN_DOMAIN}`,
-});
-
 const sendEmail = async (options) => {
+  const mg = mailgun({
+    apiKey: `${process.env.MAILGUN_APIKEY}`,
+    domain: `${process.env.MAILGUN_DOMAIN}`,
+  });
   // send mail with defined transport object
+  console.log(process.env.MAILGUN_DOMAIN);
   const data = {
-    from: `<${process.env.FROM_EMAIL}>`,
-    to: options.email,
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: 'discountappgroup@gmail.com',
     subject: options.subject,
     html: options.html,
   };
