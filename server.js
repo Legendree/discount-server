805a4e01d1f2f734d38364525087003401236b36
BannedWolf <dsapaiv@gmail.com>
2020-05-20 18:48:29 +0300

    Tryin to deploy to heroku

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.use('/v1/api/comments', comments);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+
 const server = app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`.green.bold)
 );
