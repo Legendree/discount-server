3e8cbad1e36d7a33e8cd23680c2da00329a4f7a7
BannedWolf <dsapaiv@gmail.com>
2020-05-20 18:54:16 +0300

    Little test

--- server.js
+++ server.js
@@ -73,9 +73,7 @@ app.use('/v1/api/comments', comments);
 // Error handling
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () =>
+const server = app.listen(process.env.PORT || 5000, () =>
   console.log(`Server running on port ${PORT}`.green.bold)
 );
 
